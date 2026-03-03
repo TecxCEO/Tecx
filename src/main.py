dd1cac40c94e529f611f814944ca8053e8a58daf
TecxCEO <ceotecx@gmail.com>
2026-03-04 00:56:26 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -195,7 +195,7 @@ class CubeSolver:
         # if(last_move.strip()[:2]!=to_move.strip()[:2]):
         if last_move.strip()[:2]!=to_move.strip()[:2]:
           next_moves_list.append(to_move)
-        states[i],move_path_history,puzzle_solve=moves(states[i],next_moves_list,move_path_history)
+        states[i],move_path_history,puzzle_solve=self.moves(states[i],next_moves_list,move_path_history)
       if states[i]==solution:
         puzzle_solve=True
         return states[i],move_path_history,puzzle_solve
